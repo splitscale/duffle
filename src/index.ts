cd12bae5aab4fbd54309853e0a64339001ebca4f
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-07-16 16:32:41 +0800

    fixed common js compatibility

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export const duffle = () => {};
+export { BaseHandlerRegistrator } from './baseHandlerRegistrator';
+export { BaseHandler } from './baseHandler';
+export { DuffleRequest, DuffleResponse } from './commonTypes';
+export { DuffleRequestMethods } from './duffleRequestMethods';
+export { DuffleStatus } from './duffleStatusCodes';
+export { HandlerRegistratorCollector } from './handlerRegistratorCollector';
+export { HandlerResolver } from './handlerResolver';
